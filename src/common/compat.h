7cfaec4573b94d732fe177efecc6d87fdaed48b0
Alejandro Alvarado <44826516+seisvelas@users.noreply.github.com>
2019-01-05 17:42:20 -0600

    Update compat.h

--- src/common/compat.h
+++ src/common/compat.h
@@ -155,8 +155,8 @@ void tsocks_once(tsocks_once_t *o, void (*init_routine)(void));
 #define TSOCKS_NR_CLOCK_GETTIME __NR_clock_gettime
 #define TSOCKS_NR_FORK      __NR_fork
 #define TSOCKS_NR_MEMFD_CREATE __NR_memfd_create
-#define TSOCKS_NR_getdents __NR_getdents
-#define TSOCKS_NR_getdents64 __NR_getdents64
+#define TSOCKS_NR_GETDENTS __NR_getdents
+#define TSOCKS_NR_GETDENTS64 __NR_getdents64
 
 /*
  * Despite glibc providing wrappers for these calls for a long time
